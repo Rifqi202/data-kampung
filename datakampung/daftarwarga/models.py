b21a1c0306ce210894d9a7c2d9f730f4f44b5639
Rifqi202 <febriawanrifqi@gmail.com>
2021-10-25 01:45:56 -0800

    Initial commit

---
+++ datakampung/daftarwarga/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+# class makanan (models.Model):
+#     Nama = models.CharField(max_length=200)
+#     NIK = models.IntegerField()
+#     Umur = models.IntegerField()
